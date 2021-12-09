3371974961c7be9ffc89555ca0412ae00c512dee
Peter Wilhelmsson <peter.wilhelmsson@neo4j.com>
2021-12-09 08:44:40 +0100

    Typing working

--- vic/go.mod
+++ vic/go.mod
@@ -2,4 +2,11 @@ module main
 
 go 1.16
 
-require gopkg.in/yaml.v2 v2.4.0 // indirect
+require (
+	github.com/containerd/containerd v1.5.7 // indirect
+	github.com/docker/docker v20.10.10+incompatible
+	github.com/docker/go-connections v0.4.0 // indirect
+	golang.org/x/term v0.0.0-20201126162022-7de9c90e9dd1
+	google.golang.org/grpc v1.42.0 // indirect
+	gopkg.in/yaml.v2 v2.4.0
+)
--- vic/main.go
+++ vic/main.go
@@ -1,10 +1,17 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"io"
 	"os"
 	"path"
+	"strings"
 
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/api/types/container"
+	"github.com/docker/docker/client"
+	"golang.org/x/term"
 	"gopkg.in/yaml.v2"
 )
 
@@ -40,6 +47,28 @@ type Config struct {
 	Filters map[string]RootConfig
 }
 
+func (c Config) getTag(image string) (string, error) {
+	for configuredImage, imageConfig := range c.Images {
+		if configuredImage == image {
+			return imageConfig.Tag, nil
+		}
+	}
+	return "", fmt.Errorf("No configuration for image: %s", image)
+}
+
+func (c Config) getContainerConfig(root string) (cc container.Config, err error) {
+	// Try to find a matching root
+	for configuredRoot, imageConfig := range c.Roots {
+		if strings.HasPrefix(root, configuredRoot) {
+			var tag string
+			tag, err = c.getTag(imageConfig.Image)
+			cc.Image = fmt.Sprintf("vic-%s:%s", imageConfig.Image, tag)
+			return
+		}
+	}
+	return cc, fmt.Errorf("No suitable image found for %s", root)
+}
+
 type ImageConfig struct {
 	Tag string
 }
@@ -49,20 +78,97 @@ type RootConfig struct {
 }
 
 func main() {
-	//fmt.Println(os.Args)
-	_, err := os.Getwd()
+	config, err := getConfig()
 	if err != nil {
 		panic(err)
 	}
-	//fmt.Println(wd)
-	config, err := openConfig()
+	wd, err := os.Getwd()
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println(config)
+	// Builds basic container configuration
+	containerConfig, err := config.getContainerConfig(wd)
+	if err != nil {
+		panic(err)
+	}
+	// Add more specifics to the configuration
+	containerConfig.Tty = true
+	containerConfig.AttachStderr = true
+	containerConfig.AttachStdin = true
+	containerConfig.AttachStdout = true
+	containerConfig.OpenStdin = true
+	containerConfig.StdinOnce = true
+	containerConfig.WorkingDir = "/host/code"
+
+	containerName := buildContainerId(wd)
+	fmt.Printf("Will run or exec container '%s' configures as %+v\n", containerName, containerConfig)
+	cli, err := client.NewClientWithOpts()
+	if err != nil {
+		panic(err)
+	}
+
+	hostConfig := container.HostConfig{}
+	hostConfig.AutoRemove = true
+	createResponse, err := cli.ContainerCreate(context.Background(), &containerConfig, &hostConfig, nil, nil, containerName)
+	if err != nil {
+		panic(err)
+	}
+	containerId := createResponse.ID
+
+	fd := int(os.Stdin.Fd())
+	if term.IsTerminal(fd) {
+		oldState, err := term.MakeRaw(fd)
+		if err != nil {
+			panic(err)
+		}
+		defer term.Restore(fd, oldState)
+	} else {
+		panic("Should be terminal")
+	}
+
+	attachOptions := types.ContainerAttachOptions{
+		Stdin:  true,
+		Stderr: true,
+		Stdout: true,
+		Stream: true,
+	}
+	attachResponse, err := cli.ContainerAttach(context.Background(), containerId, attachOptions)
+	if err != nil {
+		panic(err)
+	}
+	go io.Copy(os.Stdout, attachResponse.Reader)
+	//go io.Copy(os.Stderr, attachResponse.Reader)
+	go io.Copy(attachResponse.Conn, os.Stdin)
+
+	err = cli.ContainerStart(context.Background(), containerId, types.ContainerStartOptions{})
+	if err != nil {
+		panic(err)
+	}
+
+	statusCh, errCh := cli.ContainerWait(context.Background(), containerId, container.WaitConditionNotRunning)
+	select {
+	case err := <-errCh:
+		if err != nil {
+			panic(err)
+		}
+	case status := <-statusCh:
+		fmt.Println(status)
+	}
+	/*
+
+		containers, err := cli.ContainerList(context.Background(), types.ContainerListOptions{})
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println(containers)
+	*/
 }
 
-func openConfig() (config Config, err error) {
+func buildContainerId(workingDirectory string) string {
+	return "vic_" + strings.ReplaceAll(workingDirectory, "/", "_")
+}
+
+func getConfig() (config Config, err error) {
 	var (
 		dirPath  string
 		filePath string
