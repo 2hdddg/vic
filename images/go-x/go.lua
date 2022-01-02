4a534ef85432e63d13caa8a3b5fe100aa56e9f15
Peter Wilhelmsson <peter.wilhelmsson@neo4j.com>
2022-01-02 17:14:30 +0100

    Fixed go goto def

--- images/go-x/go.lua
+++ images/go-x/go.lua
@@ -1,2 +1,11 @@
 local lspconfig = require'lspconfig'
-lspconfig.gopls.setup{}
+local lspconfig_util = require'lspconfig.util'
+
+local root_dir = function(fname)
+    return lspconfig_util.root_pattern 'go.work'(fname) or lspconfig_util.root_pattern('go.mod', '.git', '/host/workspace/pkg/mod')(fname)
+end
+
+lspconfig.gopls.setup{
+    root_dir = root_dir,
+}
+vim.api.nvim_command(' autocmd BufWritePre *.go lua vim.lsp.buf.formatting_sync(nil, 1000)')
