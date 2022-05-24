49379b9c2c5eb3cad03a1a38e74cffd7336028ea
Peter Wilhelmsson <peter.wilhelmsson@neo4j.com>
2022-05-24 19:45:20 +0200

    Go: Fix problem with multiple gopls instances

--- images/go-x/go.lua
+++ images/go-x/go.lua
@@ -1,9 +1,11 @@
+
+if vim.g.goplsSetup == 1 then
+    do return end
+end
 local lspconfig = require'lspconfig'
 local util = require'lspconfig.util'
-
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
-
 lspconfig.gopls.setup{
     cmd = {"gopls", "serve"},
     filetypes = {"go", "gomod"},
@@ -11,3 +13,4 @@ lspconfig.gopls.setup{
     capabilities = capabilities,
 }
 vim.api.nvim_command(' autocmd BufWritePre *.go lua vim.lsp.buf.formatting_sync(nil, 1000)')
+vim.g.goplsSetup = 1
